446f83d258147e3595cc9853f886dfd866283dfe
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-21 22:13:03 +0300

    feat: add Button, AddIcon, and CloseIcon components for UI interactions

---
+++ src/components/icons/AddIcon.jsx
@@ -0,0 +1,18 @@
+const AddIcon = () => {
+    return (
+        <svg
+            className="add-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="currentColor"
+            viewBox="0 0 24 24"
+            aria-hidden="true"
+            focusable="false"
+        >
+            <path d="M3 13h8v8h2v-8h8v-2h-8V3h-2v8H3z"></path>
+        </svg>
+    );
+};
+
+export default AddIcon;
