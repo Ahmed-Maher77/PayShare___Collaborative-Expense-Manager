64751900a632e106be8f7b49883edf1d93fe8ff5
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-21 22:12:03 +0300

    feat: add dummy user data and associated images for user profiles

---
+++ src/assets/data/users.js
@@ -0,0 +1,29 @@
+import user_1 from "../images/user-1.jpg";
+import user_2 from "../images/user-2.jpg";
+import user_3 from "../images/user-3.jpg";
+
+const DUMMY_USERS = [
+    {
+        id: 1,
+        picture: user_1,
+        name: "Mohammad",
+        debt: -7,
+        currency: "USD",
+    },
+    {
+        id: 2,
+        picture: user_2,
+        name: "Mariam Hassan",
+        debt: 18,
+        currency: "USD",
+    },
+    {
+        id: 3,
+        picture: user_3,
+        name: "Ahmed Sayed",
+        debt: 0,
+        currency: "USD",
+    },
+];
+
+export default DUMMY_USERS;
