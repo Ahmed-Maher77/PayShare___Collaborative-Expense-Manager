998092bbc63e8947b07c536f6220cf00bc4bc872
“Ahmed <ahmedmaheraljwhry057@gmail.com>
2026-05-22 01:49:30 +0300

    feat: update Header component with logo and improved styling

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../../assets/images/logo.svg";
 
 const Header = () => {
   return (
     <header className="App-Header" role="banner" aria-label="PayShare header">
       <div className="App-Header-inner">
         <h1 className="brand">
-          Pay<span>Share</span>
+          <img className="brand-logo" src={logo} alt="PayShare logo" />
+          <span className="brand-name">
+            Pay<span>Share</span>
+          </span>
         </h1>
 
         <p className="tagline">
